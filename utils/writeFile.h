58bb47b5fe072f9b640aa4a95ed841bbd2d36958
Mengjiao Han <u1078078@utah.edu>
2019-11-24 13:58:44 -0700

    use low level api

--- utils/writeFile.h
+++ utils/writeFile.h
@@ -34,7 +34,8 @@ void writeRaw(std::vector<float> voxels, std::string fileDir, ospcommon::math::v
                     }
                 }
                 // compress sub_volume and append to file
-                int result = compress(sub_volume.data(), 4, 4, 4, 1e-3, fileDir);  
+                // int result = compress(sub_volume.data(), 4, 4, 4, 1e-3, fileDir);
+                uint result = compress_3(sub_volume.data(), fileDir);
             }
         }
     }
@@ -42,67 +43,77 @@ void writeRaw(std::vector<float> voxels, std::string fileDir, ospcommon::math::v
 }
 
 
-/* compress or decompress array */
-static int compress(float* array, int nx, int ny, int nz, float tolerance, std::string file)
+static uint compress_3(float *array, const std::string file)
 {
-    int status = 0;    /* return value: 0 = success */
-    zfp_type type;     /* array scalar type */
-    zfp_field* field;  /* array meta data */
+    uint status = 0;
     zfp_stream* zfp;   /* compressed stream */
-    void* buffer;      /* storage for compressed stream */
-    size_t bufsize;    /* byte size of compressed buffer */
-    bitstream* stream; /* bit stream to write to or read from */
-    size_t zfpsize;    /* byte size of compressed stream */
-
-    /* allocate meta data for the 3D array a[nz][ny][nx] */
-    type = zfp_type_float;
-    field = zfp_field_3d(array, type, nx, ny, nz);
-
-    /* allocate meta data for a compressed stream */
-    zfp = zfp_stream_open(NULL);
-
-    /* set compression mode and parameters via one of three functions */
-    /*  zfp_stream_set_rate(zfp, rate, type, 3, 0); */
-    /*  zfp_stream_set_precision(zfp, precision); */
-    zfp_stream_set_accuracy(zfp, tolerance);
-
-    /* allocate buffer for compressed data */
-    bufsize = zfp_stream_maximum_size(zfp, field);
-    buffer = malloc(bufsize);
-
-    // std::cout << "buffer size " << bufsize << std::endl;
-
-    /* associate bit stream with allocated buffer */
-    stream = stream_open(buffer, bufsize);
-    zfp_stream_set_bit_stream(zfp, stream);
-    zfp_stream_rewind(zfp);
-
-    /* compress or decompress entire array */
-
-    /* compress array and output compressed stream */
-    zfpsize = zfp_compress(zfp, field);
-
-    if (!zfpsize) {
-        fprintf(stderr, "compression failed\n");
-        status = 1;
-    }
-    else
-    {
-        FILE *f = fopen(file.c_str(), "ab");
-        // outFile.write(reinterpret_cast<char*>(&buffer[0]), sizeof(buffer)*sizeof(float));
-        // std::cout << "size of array after compress " << zfpsize << std::endl;
-        fwrite(buffer, 1, zfpsize, f);
-        fclose(f);
-        // fwrite(buffer, 1, zfpsize, stdout);
-    }
-
-
-    /* clean up */
-    zfp_field_free(field);
-    zfp_stream_close(zfp);
-    stream_close(stream);
-    free(buffer);
-    // free(array);
+    zfp_encode_block_float_3(zfp, array);
 
     return status;
 }
+
+
+// /* compress or decompress array */
+// static int compress(float* array, int nx, int ny, int nz, float tolerance, std::string file)
+// {
+//     int status = 0;    /* return value: 0 = success */
+//     zfp_type type;     /* array scalar type */
+//     zfp_field* field;  /* array meta data */
+//     zfp_stream* zfp;   /* compressed stream */
+//     void* buffer;      /* storage for compressed stream */
+//     size_t bufsize;    /* byte size of compressed buffer */
+//     bitstream* stream; /* bit stream to write to or read from */
+//     size_t zfpsize;    /* byte size of compressed stream */
+
+//     /* allocate meta data for the 3D array a[nz][ny][nx] */
+//     type = zfp_type_float;
+//     field = zfp_field_3d(array, type, nx, ny, nz);
+
+//     /* allocate meta data for a compressed stream */
+//     zfp = zfp_stream_open(NULL);
+
+//     /* set compression mode and parameters via one of three functions */
+//     /*  zfp_stream_set_rate(zfp, rate, type, 3, 0); */
+//     /*  zfp_stream_set_precision(zfp, precision); */
+//     zfp_stream_set_accuracy(zfp, tolerance);
+
+//     /* allocate buffer for compressed data */
+//     bufsize = zfp_stream_maximum_size(zfp, field);
+//     buffer = malloc(bufsize);
+
+//     // std::cout << "buffer size " << bufsize << std::endl;
+
+//     /* associate bit stream with allocated buffer */
+//     stream = stream_open(buffer, bufsize);
+//     zfp_stream_set_bit_stream(zfp, stream);
+//     zfp_stream_rewind(zfp);
+
+//     /* compress or decompress entire array */
+
+//     /* compress array and output compressed stream */
+//     zfpsize = zfp_compress(zfp, field);
+
+//     if (!zfpsize) {
+//         fprintf(stderr, "compression failed\n");
+//         status = 1;
+//     }
+//     else
+//     {
+//         FILE *f = fopen(file.c_str(), "ab");
+//         // outFile.write(reinterpret_cast<char*>(&buffer[0]), sizeof(buffer)*sizeof(float));
+//         // std::cout << "size of array after compress " << zfpsize << std::endl;
+//         fwrite(buffer, 1, zfpsize, f);
+//         fclose(f);
+//         // fwrite(buffer, 1, zfpsize, stdout);
+//     }
+
+
+//     /* clean up */
+//     zfp_field_free(field);
+//     zfp_stream_close(zfp);
+//     stream_close(stream);
+//     free(buffer);
+//     // free(array);
+
+//     return status;
+// }
